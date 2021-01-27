be29e78f8aa4a665dee50291bc9cb74e26652e7e
Ihor Cheberiak <Groomsha@users.noreply.github.com>
2021-01-27 20:26:57 +0200

    fix scripts

--- backup/backup_kvm_vm_in_lvm.py
+++ backup/backup_kvm_vm_in_lvm.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-version: 0.7
+version: 0.9
 
 Скрипт позволяет делать автоматические бекапы виртуальных машин (VM)
 (через cron) в гипервизоре KVM размещенных на блочном устройстве LVM
@@ -10,7 +10,6 @@ version: 0.7
 import os as terminal_os
 import sys as system_py
 import time as time_os
-import subprocess as sub_os
 
 kvm_vm_os = ""
 kvm_vm_name = ""
@@ -19,7 +18,7 @@ dir_logs = ""
 
 folder_backup = kvm_vm_name + "-" + kvm_vm_os + "_" + time_os.strftime("%d.%m.%Y")
 touch_folder_src = dir_backup + folder_backup + "/" + kvm_vm_name + "-" + kvm_vm_os
-touch_lvm_src = []
+touch_lvm_src = terminal_os.popen("virsh domblklist " + kvm_vm_name + " --details").read().split()
 
 
 def returning_command(command):
@@ -43,21 +42,20 @@ def virsh_command():
     """ Остонавливает виртуальную машину (VM) и собирает информацию
         для ее восстановления из Backup по надобности в будущем.
     """
-    touch_lvm_src = terminal_os.popen("sudo virsh domblklist " + kvm_vm_name + " --details").read().split()
-
-    if terminal_os.popen("sudo virsh domstate " + kvm_vm_name).read().split() == ["работает"]:
-        terminal_os.popen("sudo virsh save " + kvm_vm_name + " " + touch_folder_src + ".vmstate" + " --running")
-    elif terminal_os.popen("sudo virsh domstate " + kvm_vm_name).read().split() == ["выключен"]:
-        terminal_os.popen("sudo virsh dumpxml " + kvm_vm_name + " > " + touch_folder_src + ".xml") 
-        terminal_os.popen("sudo virsh domblkinfo " + kvm_vm_name + " " + touch_lvm_src[8] + " > " + touch_folder_src + 
-                       "-raw_info" + " && " + "sudo virsh vol-pool " + touch_lvm_src[8] + " >> " + touch_folder_src + 
+    if terminal_os.popen("virsh domstate " + kvm_vm_name).read().split() == ["работает"]:
+        terminal_os.popen("virsh save " + kvm_vm_name + " " + touch_folder_src + ".vmstate --running").read()
+        logs_creation("Process Create " + kvm_vm_name + ".vmstate --running ======>> Завершон!")
+    if terminal_os.popen("virsh domstate " + kvm_vm_name).read().split() == ["выключен"]:
+        terminal_os.popen("virsh dumpxml " + kvm_vm_name + " > " + touch_folder_src + ".xml") 
+        terminal_os.popen("virsh domblkinfo " + kvm_vm_name + " " + touch_lvm_src[8] + " > " + touch_folder_src + 
+                       "-raw_info" + " && " + "virsh vol-pool " + touch_lvm_src[8] + " >> " + touch_folder_src + 
                        "-raw_info" + " && " + "echo " + touch_lvm_src[8] + " >> " + touch_folder_src + "-raw_info")
 
 
 def virsh_restore():
     """ Запускает виртуальную машину (VM) из сохраненного ранее состояния """
-    if terminal_os.popen("sudo virsh domstate " + kvm_vm_name).read().split() == ["выключен"]:
-        terminal_os.popen("sudo virsh restore " + touch_folder_src + ".vmstate")
+    if terminal_os.popen("virsh domstate " + kvm_vm_name).read().split() == ["выключен"]:
+        terminal_os.popen("virsh restore " + touch_folder_src + ".vmstate")
         logs_creation("Process Restore VM: Начат процесс восстановления состояния вертуальной машины!")
     else:
         logs_creation("Error Process Restore VM: Виртуальная машина не выключена, процесс восстановления прерван!")
@@ -67,30 +65,30 @@ def archive_creation(compression = 3):
     """ compression: Степень сжатия .gz файла от 1 до 9. Чем выше степень,
         тем больше нужно мощностей процессора и времени на создание архива.
     """
-    terminal_os.popen("sudo dd if=" + touch_lvm_src + "_snap" + " | gzip -ck -" + str(compression) + " > " + touch_folder_src + ".gz").read()
+    terminal_os.popen("dd if=" + touch_lvm_src[8] + "_snap" + " | gzip -ck -" + str(compression) + " > " + touch_folder_src + ".gz").read()
 
 
 def lvm_command(command, ratio = 2):
     """ command: (1) Создать LVM_Snap. (2) Удалить LVM_Snap. (3) Информация по LVM_Snap
         ratio: Размер таблицы(буфера), на каждые 8Gb LVM c VM нужно 256M. ratio=2(16Gb LVM) 
     """
-    if command == 1:
-        terminal_os.popen("sudo lvcreate -s -n " + touch_lvm_src + "_snap -L" + str(ratio*256) + "M " + touch_lvm_src)
-        logs_creation("LVM Create " + touch_lvm_src + "_snap Size: " + str(ratio*256) + "M Target:" + touch_lvm_src)
-    elif command == 2:
-        terminal_os.popen("sudo lvremove -f " + touch_lvm_src + "_snap")
-        logs_creation("LVM Remove " + touch_lvm_src + "_snap")
-    elif command == 3:
-        return terminal_os.popen("sudo lvdisplay " + touch_lvm_src + "_snap").read()
+    if command == "create":
+        terminal_os.popen("lvcreate -s -n " + touch_lvm_src[8] + "_snap -L" + str(ratio*256) + "M " + touch_lvm_src[8]).read()
+        logs_creation("LVM Create " + touch_lvm_src[8] + "_snap Size: " + str(ratio*256) + "M Target:" + touch_lvm_src[8])
+    elif command == "remove":
+        logs_creation(terminal_os.popen("lvdisplay " + touch_lvm_src[8] + "_snap").read())
+        terminal_os.popen("lvremove -f " + touch_lvm_src[8] + "_snap")
+        logs_creation("LVM Remove " + touch_lvm_src[8] + "_snap")
 
 
-logs_creation("Start Process Backup VM: " + kvm_vm_name)
-returning_command("mkdir -p " + dir_backup + folder_backup + "/")
-logs_creation("Create Folder Backup VM: " + dir_backup + folder_backup)
-#virsh_command()
-#lvm_command(1,3)
-#virsh_restore()
-#archive_creation(3)
-#lvm_command(3)
-#lvm_command(2)
-print("################")
+main()
+def main():
+    logs_creation("Start Process Backup VM: " + kvm_vm_name)
+    returning_command("mkdir -p " + dir_backup + folder_backup + "/")
+    logs_creation("Create Folder Backup VM: " + dir_backup + folder_backup)
+
+    virsh_command()
+    lvm_command("create", 3)
+    virsh_restore()
+    archive_creation(3)
+    lvm_command("remove")
