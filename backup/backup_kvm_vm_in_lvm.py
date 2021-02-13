e92792973f83b2585a5bb965d57abddbb50fffcf
Ihor Cheberiak <Groomsha@users.noreply.github.com>
2021-02-13 18:44:43 +0200

    fix backup scripts

--- backup/backup_kvm_vm_in_lvm.py
+++ backup/backup_kvm_vm_in_lvm.py
@@ -2,7 +2,7 @@
 
 """
 Project Name: 'basic-scripts'
-Version: 1.1с
+Version: 1.2
 
 Description: Скрипт позволяет делать автоматические бекапы виртуальных машин 
 используя его в CRON для гипервизора KVM размещенных на блочном устройстве LVM.
@@ -11,37 +11,51 @@ Ihor Cheberiak (c) 2021
 https://www.linkedin.com/in/ihor-cheberiak/
 """
 
-import sys as path_os
 import time as time_os
 import os as terminal_os
+import argparse as parser
 import subprocess as shell
 
 
+help_parser = parser.ArgumentParser(description="KVM backup of Virtual Machines on the LVM block device")
+
+help_parser.add_argument("vm_name", type=str, help="Example: srv4prod-vm")
+help_parser.add_argument("lvm_dev", type=str,  help="Example: /dev/vg0/prod-vm")
+help_parser.add_argument("-size_snap", type=int, default=2, help="Example: default=2 is 512M Snapshot for VMs less than 16Gb")
+help_parser.add_argument("bakup_folder", type=str, help="Example: /var/backup/prod-vm/")
+
+args_parser = help_parser.parse_args()
+print(args_parser)
+
 dir_logs = "/var/log/"
-kvm_vm_name, dev_lvm, size_snap, dir_backup = path_os.argv[1:]
+kvm_vm_name = args_parser.vm_name
+dev_lvm = args_parser.lvm_dev
+size_snap = args_parser.size_snap
+dir_backup = args_parser.bakup_folder
+
 folder_backup = kvm_vm_name + "_" + time_os.strftime("%d.%m.%Y")
 touch_folder_src = dir_backup + folder_backup + "/" + kvm_vm_name
 
 
 def main():
-    performance_shell("mkdir -p {}{}/".format(dir_backup, folder_backup))
-    logs_creation(["Start Process Backup Virtual Machine: {} {}{}".format(kvm_vm_name, dir_backup, folder_backup)])
+    performance_shell("mkdir -p {0}{1}/".format(dir_backup, folder_backup))
+    logs_creation(["Start Process Backup Virtual Machine: {0} {1}{2}".format(kvm_vm_name, dir_backup, folder_backup)])
     
     virsh_command()
     lvm_command("create", int(size_snap))
     virsh_restore()
 
-    logs_creation(["#"*100])
+    logs_creation(["#"*120])
     terminal_os._exit(0)
 
 
 def logs_creation(messages):
-    if terminal_os.path.isfile("{}{}.log".format(dir_logs, kvm_vm_name)):
+    if terminal_os.path.isfile("{0}{1}.log".format(dir_logs, kvm_vm_name)):
         access_type = "a"
     else:
         access_type = "w"
     
-    with open("{}{}.log".format(dir_logs, kvm_vm_name), access_type) as log:
+    with open("{0}{1}.log".format(dir_logs, kvm_vm_name), access_type) as log:
         for message in messages:
             log.write("\n" + time_os.ctime() + " " + message)
 
@@ -54,22 +68,22 @@ def performance_shell(command, wait_shell = True):
     
     output, errors = shell_os.communicate()
     if len(str(output)) != 0:
-        logs_creation(str(output).splitlines())
+        logs_creation(str(output.strip()).splitlines())
     if len(str(errors)) != 0:
-        logs_creation(str(errors).splitlines())
+        logs_creation(str(errors.strip()).splitlines())
 
 
 def virsh_command():
     """ Остонавливает виртуальную машину (VM) и собирает информацию
         для ее восстановления из Backup по надобности в будущем.
     """
-    if terminal_os.popen("virsh domstate " + kvm_vm_name).read().split() == ["running"]:
-        performance_shell("virsh save {} {}.vmstate --running".format(kvm_vm_name, touch_folder_src))
-    if terminal_os.popen("virsh domstate " + kvm_vm_name).read().split() != ["running"]:
-        performance_shell("virsh dumpxml {} > {}.xml".format(kvm_vm_name, touch_folder_src))
-        performance_shell("virsh domblkinfo {} {} > {}-raw_info && virsh vol-pool {} >> {}-raw_info && echo {} >> {}-raw_info".format(kvm_vm_name, dev_lvm, touch_folder_src, dev_lvm, touch_folder_src, dev_lvm, touch_folder_src))
+    if terminal_os.popen("virsh domstate " + kvm_vm_name).read().split() == ["работает"] :
+        performance_shell("virsh save {0} {1}.vmstate --running".format(kvm_vm_name, touch_folder_src))
+    if terminal_os.popen("virsh domstate " + kvm_vm_name).read().split() != ["работает"]:
+        performance_shell("virsh dumpxml {0} > {1}.xml".format(kvm_vm_name, touch_folder_src))
+        performance_shell("virsh domblkinfo {0} {1} > {2}-raw_info && virsh vol-pool {1} >> {2}-raw_info && echo {1} >> {2}-raw_info".format(kvm_vm_name, dev_lvm, touch_folder_src))
     
-    logs_creation(["Process Virsh Create: {}.vmstate --running and creation of auxiliary files VM!".format(kvm_vm_name)])
+    logs_creation(["Process Virsh Create: {0}.vmstate --running and creation of auxiliary files VM!".format(kvm_vm_name)])
 
 
 def lvm_command(command, ratio = 2):
@@ -78,22 +92,22 @@ def lvm_command(command, ratio = 2):
         ratio=2 это 512M Snapshot для VM размером меньше чем 16Gb
     """
     if command == "create":
-        performance_shell("sudo lvcreate -s -n {}_snap -L{}M {}".format(dev_lvm, str(ratio*256), dev_lvm))
-        logs_creation(["LVM Snapshot Create: {}_snap Size: {}M Target: {}".format(dev_lvm, str(ratio*256), dev_lvm)])
+        performance_shell("sudo lvcreate -s -n {0}_snap -L{1}M {0}".format(dev_lvm, str(ratio*256)))
+        logs_creation(["LVM Snapshot Create: {0}_snap Size: {1}M Target: {0}".format(dev_lvm, str(ratio*256))])
     elif command == "remove":
-        performance_shell("sudo lvremove -f {}_snap".format(dev_lvm))
-        logs_creation(["LVM Snapshot Remove {}_snap".format(dev_lvm)])
+        performance_shell("sudo lvremove -f {0}_snap".format(dev_lvm))
+        logs_creation(["LVM Snapshot Remove {0}_snap".format(dev_lvm)])
 
 
 def virsh_restore():
     """ Запускает виртуальную машину (VM) из сохраненного ранее состояния """
-    if terminal_os.popen("virsh domstate " + kvm_vm_name).read().split() != ["running"]:
-        logs_creation(["Start Process Restore Virtual Machine: {}.vmstate --running".format(kvm_vm_name)])
-        performance_shell("virsh restore {}.vmstate".format(touch_folder_src))
+    if terminal_os.popen("virsh domstate " + kvm_vm_name).read().split() != ["работает"]:
+        logs_creation(["Start Process Restore Virtual Machine: {0}.vmstate --running".format(kvm_vm_name)])
+        performance_shell("virsh restore {0}.vmstate".format(touch_folder_src))
         archive_creation()
     else:
         logs_creation(["Error Process Restore VM: The VM is not turned off, removing the folder with oriental information!"])
-        performance_shell("rm -r {}{}/".format(dir_backup, folder_backup))
+        performance_shell("rm -r {0}{1}/".format(dir_backup, folder_backup))
         lvm_command("remove")
         terminal_os._exit(1)
 
@@ -103,10 +117,10 @@ def archive_creation(compression = 3):
         тем больше нужно мощностей процессора и времени на создание архива.
     """
     logs_creation(["Process GZIP LVM Snapshot: For disk recovery Virtual Machine " + kvm_vm_name])
-    performance_shell("dd if={}_snap | gzip -ck -{} > {}.gz".format(dev_lvm, str(compression), touch_folder_src))
+    performance_shell("dd if={0}_snap | gzip -ck -{1} > {2}.gz".format(dev_lvm, str(compression), touch_folder_src))
 
     logs_creation(["Allocated to LVM Snapshot: Allocated should be < 100% for performance Snapshot!"])
-    performance_shell("sudo lvdisplay {}_snap".format(dev_lvm))
+    performance_shell("sudo lvdisplay {0}_snap".format(dev_lvm))
 
     lvm_command("remove")
 
--- backup/restore_kvm_vm_in_lvm.py
+++ backup/restore_kvm_vm_in_lvm.py
@@ -2,9 +2,10 @@
 
 """
 Project Name: 'basic-scripts'
-Version: 0.9
+Version: 1.0
 
-Description:
+Description: Скрипт позволяет востоновить бекапы виртуальных 
+машин для гипервизора KVM размещенных на блочном устройстве LVM.
 
 Ihor Cheberiak (c) 2021
 https://www.linkedin.com/in/ihor-cheberiak/
@@ -12,42 +13,51 @@ https://www.linkedin.com/in/ihor-cheberiak/
 
 import time as time_os
 import os as terminal_os
+import argparse as parser
 import subprocess as shell
 
+help_parser = parser.ArgumentParser(description="KVM restoring of Virtual Machines on the LVM block device")
+help_parser.add_argument("-bakup", default=None, type=str, help="Example: /var/backup/prod-vm/srv-test-vm_01.01.2077/")
 
-dir_backup = ""
-kvm_vm_name = ""
+args_parser = help_parser.parse_args()
+dir_backup = args_parser.bakup
+print(args_parser)
+
+dir_backup = input("Enter the path to the directory with the backup of the desired VM: ")
+kvm_vm_name = str(dir_backup.split("/")[-2])[:-11]
 dir_logs = "/var/log/"
 
 
 def main():
-    dir_backup = input("Enter the path to the directory with the backup of the desired VM: ")
-    kvm_vm_name = str(dir_backup.split("/")[-2])[:-11]
-
-    with open("{}{}-raw_info".format(dir_backup, kvm_vm_name)) as backup:
+    with open("{0}{1}-raw_info".format(dir_backup, kvm_vm_name)) as backup:
         temp_str = ""
         for line in backup:
             temp_str += line
 
     *rest, size_lvm, _, dev_lvm = temp_str.split()
 
+    logs_creation(["Start Process Restoring Virtual Machine: {0} {1}".format(kvm_vm_name, dir_backup)])
+
     virsh_command("destroy")
     lvm_command("remove", dev_lvm)
     virsh_command("define")
     lvm_command("create", dev_lvm, size_lvm)
 
-    logs_creation(["#"*100])
+    logs_creation(["#"*120])
+    print("Restore Сompleted!")
+    
     terminal_os._exit(0)
 
 
 def logs_creation(messages):
-    if terminal_os.path.isfile("{}{}.log".format(dir_logs, kvm_vm_name)):
+    if terminal_os.path.isfile("{0}{1}.log".format(dir_logs, kvm_vm_name)):
         access_type = "a"
     else:
         access_type = "w"
     
-    with open("{}{}.log".format(dir_logs, kvm_vm_name), access_type) as log:
+    with open("{0}{1}.log".format(dir_logs, kvm_vm_name), access_type) as log:
         for message in messages:
+            print(time_os.ctime() + " " + message)
             log.write("\n" + time_os.ctime() + " " + message)
 
 
@@ -59,20 +69,21 @@ def performance_shell(command, wait_shell = True):
     
     output, errors = shell_os.communicate()
     if len(str(output)) != 0:
-        logs_creation(str(output).splitlines())
+        logs_creation(str(output.strip()).splitlines())
     if len(str(errors)) != 0:
-        logs_creation(str(errors).splitlines())
+        logs_creation(str(errors.strip()).splitlines())
 
 
 def virsh_command(command):
-    """ 
+    """ Уничтажает виртуальную машину (VM), восстановление 
+        из Backup и запускает виртуальную машину (VM)
     """
     if command == "destroy":
-        performance_shell("virsh destroy {}".format(kvm_vm_name))
+        performance_shell("virsh destroy {0}".format(kvm_vm_name))
     elif command == "define":
-        performance_shell("virsh define {}{}.xml".format(dir_backup, kvm_vm_name))
+        performance_shell("virsh define {0}{1}.xml".format(dir_backup, kvm_vm_name))
     elif command == "restore":
-        performance_shell("virsh restore {}{}.vmstate".format(dir_backup, kvm_vm_name))
+        performance_shell("virsh restore {0}{1}.vmstate".format(dir_backup, kvm_vm_name))
 
 
 def lvm_command(command, lvm, size = None):
@@ -80,14 +91,17 @@ def lvm_command(command, lvm, size = None):
         size: Размер блочного устройства для Virtual Machine в 'Байтах'. Из файла -raw_info. 
     """
     if command == "create":
-        performance_shell("sudo lvcreate -n {} -L{}B {}".format(kvm_vm_name, str(size), lvm.split("/")[-2]))
+        performance_shell("sudo lvcreate -y -n {0} -L{1}B {2}".format(kvm_vm_name, str(size), lvm.split("/")[-2]))
+        logs_creation(["LVM Block Device Create: {0} Size: {1} Byte Target: {2}".format(kvm_vm_name, str(size), lvm)])
         archive_creation(lvm)
     elif command == "remove":
-        performance_shell("sudo lvremove -f {}".format(lvm))
+        performance_shell("sudo lvremove -f {0}".format(lvm))
+        logs_creation(["LVM Block Device Remove {0}".format(lvm)])
 
 
 def archive_creation(lvm):
-    performance_shell("gunzip -ck {}{}.gz > {}".format(dir_backup, kvm_vm_name, lvm))
+    logs_creation(["Process GUNZIP LVM Block Device: For disk recovery VM: " + kvm_vm_name])
+    performance_shell("gunzip -ck {0}{1}.gz > {2}".format(dir_backup, kvm_vm_name, lvm))
     virsh_command("restore")
 
 
